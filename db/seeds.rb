999e621be89dab35050c92ca454d2f4ec2c2bf4a
Jye Harry <jyeharry21@hotmail.com>
2021-01-25 12:30:49 +1100

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# User.destroy_all
+# u1 = User.create :email => 'jyeharry21@hotmail.com', :password => 'chicken', :first_name => 'Jye', :last_name => 'Harry'
+# puts "#{User.count} users"
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Bar.destroy_all
+# b1 = Bar.create :name => ''
