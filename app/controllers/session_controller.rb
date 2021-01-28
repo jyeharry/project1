943247b05145e461c7b23050a7ec65a25133b939
Jye Harry <jyeharry21@hotmail.com>
2021-01-29 10:45:07 +1100

    added an error message for the sign up page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:error]
+      flash[:error] = "Invalid email or password"
       redirect_to login_path
     end
   end
