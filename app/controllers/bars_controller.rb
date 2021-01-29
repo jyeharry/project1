ed7260baa56b7cb78cce0bd8cf246032b812aa9f
Jye Harry <jyeharry21@hotmail.com>
2021-01-29 12:31:36 +1100

    added an error to display when search is invalid

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -5,26 +5,31 @@ class BarsController < ApplicationController
 
   def search
     coords = Geocoder.search(params[:location])
-    
-    coords = coords.first.coordinates
-    client = GooglePlaces::Client.new(ENV["GOOGLE_PLACES_API_KEY"])
-    spots = client.spots(coords[0], coords[1], :types => ['bar', 'night_club'], :detail => true);
-    spots.each do |spot|
-      bar = Bar.new
-      bar.name = spot.name
-      bar.address = spot.formatted_address
-      bar.phone = spot.formatted_phone_number
-      unless spot.photos.empty?
-        bar.image = spot.photos[0].fetch_url(400)
-      else
-        bar.image = ActionController::Base.helpers.image_path('stock-photo.jpg')
+    if coords.empty?
+      flash[:error_invalid_search] = 'That location could not be found. Please check the spelling and try again.'
+      redirect_to bars_path
+    else
+      flash[:error_invalid_search] = nil
+      coords = coords.first.coordinates
+      client = GooglePlaces::Client.new(ENV["GOOGLE_PLACES_API_KEY"])
+      spots = client.spots(coords[0], coords[1], :types => ['bar', 'night_club'], :detail => true);
+      spots.each do |spot|
+        bar = Bar.new
+        bar.name = spot.name
+        bar.address = spot.formatted_address
+        bar.phone = spot.formatted_phone_number
+        unless spot.photos.empty?
+          bar.image = spot.photos[0].fetch_url(400)
+        else
+          bar.image = ActionController::Base.helpers.image_path('stock-photo.jpg')
+        end
+        bar.city = spot.city
+        bar.link = spot.website
+        bar.bar_id = spot.place_id
+        bar.save
       end
-      bar.city = spot.city
-      bar.link = spot.website
-      bar.bar_id = spot.place_id
-      bar.save
+      redirect_to bars_path
     end
-    redirect_to bars_path
   end
 
   private
